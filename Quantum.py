be2a7b1213b0eeedf204589e3ac207393c32f78e
ROAD-28 <sammyy4028@gmail.com>
2025-07-11 13:55:20 -0500

    ALMOST DONEEEE

--- Quantum.py
+++ Quantum.py
@@ -1,22 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy
+import itertools
+from math import radians, degrees
+from scipy.optimize import minimize
+
 import cirq
-def create_quantum_circuit():
-    # Create a quantum circuit with 2 qubits
-    qubit1 = cirq.GridQubit(0, 0)
-    qubit2 = cirq.GridQubit(0, 1)
+import numpy as np
+from scipy.optimize import minimize
+
+n_qubit = 2
+qubits = cirq.NamedQubit.range(n_qubits)
+#(for those who don't know, ansatz is a trail wave function, search it up)
+def ansatz(params):
     circuit = cirq.Circuit()
-
-    # Add gates to the circuit
-    circuit.append([cirq.H(qubit1), cirq.CNOT(qubit1, qubit2)])
-
-    # Measure the qubits
-    circuit.append(cirq.measure(qubit1, key='q1'), cirq.measure(qubit2, key='q2'))
-
+    for i, qubit in enumerate(qubits):
+        circuit.append(cirq.rx(params[i])(qubit))
+ 
     return circuit
-def simulate_quantum_circuit(circuit):
-    # Create a simulator
-    simulator = cirq.Simulator()
 
-    # Simulate the circuit
-    result = simulator.run(circuit, repetitions=10)
 
-    return result
+
+
+def protein_folding_hamiltonian():
+    # placeholder cause idk how to do hamiltonian yet
+    pass
+
+
+
+init_params = np.random.rand(n_qubits)
+result = minimize(expectation, init_params, method='COBYLA')
+print("Minimum energy:", result.fun)
